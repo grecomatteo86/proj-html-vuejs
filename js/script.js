703b67d823595bd3776087e351d61219b64cae39
Matteo <grecomatteo86@gmail.com>
2021-04-20 10:44:40 +0200

    Vue dynamic bottom_nav_bar links

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-
+var app = new Vue (
+  {
+    el : '#app',
+    data :{
+      bottomNavLinks:[
+        {
+          title:'home',
+          link:'#'
+        },
+        {
+          title:'raters',
+          link:'#'
+        },
+        {
+          title:'testimonials',
+          link:'#'
+        },
+        {
+          title:'FAQ',
+          link:'#'
+        },
+        {
+          title:'blog',
+          link:'#'
+        },
+        {
+          title:'contact',
+          link:'#'
+        }
+      ]
+    }
+  }
+)
